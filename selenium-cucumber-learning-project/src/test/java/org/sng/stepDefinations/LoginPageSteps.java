6dc95468782c71148a9205e4238bf5f10ea8f98f
sukriti kusumbia <sukritikusumbia@sukritis-MacBook-Pro.local>
2023-05-31 20:10:39 +0530

    Add feature file for login page

--- selenium-cucumber-learning-project/src/test/java/org/sng/stepDefinations/LoginPageSteps.java
+++ selenium-cucumber-learning-project/src/test/java/org/sng/stepDefinations/LoginPageSteps.java
@@ -1,5 +1,43 @@
 package org.sng.stepDefinations;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginPageSteps {
+
+  @Given("user is on login page")
+  public void user_is_on_login_page() {
+
+  }
+  @When("user gets the title of the page")
+  public void user_gets_the_title_of_the_page() {
+
+  }
+  @Then("page title should be {string}")
+  public void page_title_should_be(String string) {
+  }
+
+
+  @Then("forgot your password link should be displayed on the login page")
+  public void forgot_your_password_link_should_be_displayed_on_the_login_page() {
+  }
+
+
+  @When("user enters username {string}")
+  public void user_enters_username(String string) {
+  }
+
+  @When("user enters password {string}")
+  public void user_enters_password(String string) {
+  }
+
+  @When("user clicks on login button")
+  public void user_clicks_on_login_button() {
+  }
+
+  @Then("user get the title of the home page")
+  public void user_get_the_title_of_the_home_page() {
+  }
 
 }
--- selenium-cucumber-learning-project/src/test/resources/features/loginPage.feature
+++ selenium-cucumber-learning-project/src/test/resources/features/loginPage.feature
@@ -2,18 +2,18 @@ Feature: Login page feature
 
   Scenario: Login page title
     Given user is on login page
-    When user gets the titile of the page
-    Then page title should be "Account Login"
+    When user gets the title of the page
+    Then page title should be "Customer Login"
 
   Scenario: Forgot password link
     Given user is on login page
-    Then forgot link should be displayed on the login page
+    Then forgot your password link should be displayed on the login page
 
   Scenario: Login with correct credentials
     Given user is on login page
     When user enters username "sk26090@gmail.com"
-    And user enters password "kdfjh"
+    And user enters password "Qwerty@123"
     And user clicks on login button
-    Then user get the title of the page
-    And page title should be "My Account"
+    Then user get the title of the home page
+    And page title should be "Home Page"
 
